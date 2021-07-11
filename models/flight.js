c903da25f8ed5d552713e6464bde43b29f4982f5
jwcrosby <jwcrosby89@gmail.com>
2021-07-10 18:05:15 -0600

    add ticket model

--- models/flight.js
+++ models/flight.js
@@ -7,6 +7,20 @@ export {
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
 
+// Define the schema/blueprint for a "ticket"
+const ticketsSchema = new Schema({
+    seat: {
+        type: String, 
+        match: /[A-F][1-9]\d?/
+    },
+    price: {
+        type: Number, min: 0}
+},  {
+    timestamps: true
+})
+
+
+// Define the schema/blueprint for a "flight"
 const flightSchema = new Schema({
     airline: {
         type: String, 
@@ -28,10 +42,12 @@ const flightSchema = new Schema({
         type: Date, 
         required: false, 
         // default: one year from created
-    }
+    },
+    tickets: [ticketsSchema]
 }, 
 {
     timestamps: true
 })
 
+//Create a Mongoose model of that flightSchema
 const Flight = mongoose.model("Flight", flightSchema)
